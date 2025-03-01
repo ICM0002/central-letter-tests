9e21e6770a6874028b5d7b9c1631d637b331a453
Ihor Bobulych <ihorbobuly@gmail.com>
2025-03-01 17:19:03 +0200

    fix tests

--- test_central_letter.py
+++ test_central_letter.py
@@ -5,6 +5,6 @@ def test_central_letter():
     assert central_letter("andEkas") == "E"
     assert central_letter("Termos") == False
     assert central_letter("python") == False  # Even-length word
-    assert central_letter("middle") == "d"  # Odd-length word
+    assert central_letter("middles") == "d"  # Odd-length word
     assert central_letter("a") == "a"  # Single character case
     assert central_letter("") == False  # Empty string case
